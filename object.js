54c976eb0d5a10459051364c4199b85648812858
nagamaninaga <111356935+nagamaninaga@users.noreply.github.com>
2022-08-23 08:23:16 -0400

    Add files via upload

---
+++ object.js
@@ -0,0 +1,35 @@
+
+{
+    "id": "1",
+    "details": [
+        {
+        "name": "kumar",
+        "person": "1",
+        "age": 24
+      },
+      {
+        "name": "mani",
+        "person": "1",
+        "age": 5
+      }
+    ],
+  }
+
+  {
+    "id": "2",
+    "details": [
+        {
+        "name": "kumar",
+        "person": "1",
+        "age": 24
+      },
+      {
+        "name": "vel",
+        "person": "1",
+        "age": 5
+      }
+    ],
+  }
+
+
+  
